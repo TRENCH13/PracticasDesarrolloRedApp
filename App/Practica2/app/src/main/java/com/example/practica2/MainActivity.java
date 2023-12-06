f6cff3928c5895aefff0db1c59a7ca9204f5d64d
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-12-05 19:50:25 -0600

    Segunda Práctica
    Se subió la segunda práctica que se tenía local al repositorio
    
    Práctica3- Data Binding

---
+++ App/Practica2/app/src/main/java/com/example/practica2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.practica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import com.example.practica2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //botón de calcular
+        binding.button.setOnClickListener(v->{
+            try {
+                double peso = Double.parseDouble(binding.userWeight.getText().toString());
+                double altura = Double.parseDouble(binding.userHeight.getText().toString());
+                binding.textResult.setText(getString(R.string.result_message, calc_imc(peso,altura)));
+            }catch(NumberFormatException ex){
+                Log.e("MainActivity",ex.toString());
+                Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    //calular la masa corporal
+    private double calc_imc(double peso, double altura){
+        double altura_m = altura/100;
+        return peso/(altura_m*altura_m);
+    }
+}
