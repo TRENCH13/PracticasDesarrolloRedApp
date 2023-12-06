f7ffd79a33e073a40fdd63536ea740b879520e83
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-12-05 21:36:15 -0600

    Quinta Práctica
    Se subió la quinta práctica que se tenía local al repositorio
    
    Práctica6- ViewModel y LiveData

---
+++ App/Scores/app/src/main/java/com/example/scores/MainViewModel.java
@@ -0,0 +1,41 @@
+package com.example.scores;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+
+public class MainViewModel extends ViewModel {
+    private final MutableLiveData<Integer> localScore = new MutableLiveData<>();
+    private final MutableLiveData<Integer> visitorScore = new MutableLiveData<>();
+    public MainViewModel() {
+        resetScores();
+    }
+    public LiveData<Integer> getLocalScore() {
+        return localScore;
+    }
+    public LiveData<Integer> getVisitorScore() {
+        return visitorScore;
+    }
+    void resetScores() {
+        localScore.setValue(0);
+        visitorScore.setValue(0);
+    }
+    void addPointsToScore(int points, Boolean isLocal) {
+        if (isLocal) {
+            localScore.setValue(localScore.getValue() + points);
+        } else {
+            visitorScore.setValue(visitorScore.getValue() + points);
+        }
+    }
+    void decreaseLocal() {
+        if (localScore.getValue() > 0) {
+            localScore.setValue(localScore.getValue() - 1);
+        }
+    }
+    void decreaseVisitor() {
+        if (visitorScore.getValue() > 0) {
+            visitorScore.setValue(visitorScore.getValue() - 1);
+        }
+    }
+}
+
